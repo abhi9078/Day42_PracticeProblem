18826394c25636670501de266006bef6dfd846de
Abhilash <abhilashmeher1234@gmail.com>
2022-04-27 12:56:57 +0530

    Added program file for UC_1

--- program.js
+++ program.js
@@ -1,6 +1,8 @@
 const Is_Absent=0;
-
+//To generate random number 
 let empCheck=Math.floor(Math.random()*10)%2;
+
+//if condition for printing result
 if (empCheck==0){
     console.log("Employee is Absent");
 }
